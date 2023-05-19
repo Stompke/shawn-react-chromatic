91b8cfcb46ce25f2af2a53775b12735f0d701657
Shawn Tompke <shawntompke@gmail.com>
2023-05-19 10:24:36 -0400

    my name in the circle

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -6,7 +6,7 @@ export const Circle = () => {
 
     return(
         <div style={{background: 'green', color: 'white', width: '50px', borderRadius: 100, border: 'solid grey 2px', padding: 10, justifyContent: 'space-around', alignItems: 'center', display: 'flex'}}>
-            this is a circle 
+            shawns circle
         </div>
     )
 }
