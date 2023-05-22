88b0cafced2ecb4fc86fbdf4cb8b8069528d7735
Shawn Tompke <shawntompke@gmail.com>
2023-05-22 10:38:12 -0400

    added big image

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -7,6 +7,8 @@ export const Circle = () => {
     return(
         <div style={{background: 'green', color: 'white', width: '50px', borderRadius: 100, border: 'solid grey 2px', padding: 10, justifyContent: 'space-around', alignItems: 'center', display: 'flex'}}>
             shawns circle
+            <img src="https://images.unsplash.com/photo-1684638729698-f943a755c16a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80" alt="" className="source" />
+
         </div>
     )
 }
